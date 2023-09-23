ed9a14ba98f294935bf53a94f589b948667570ec
Augusto Carrasco <142824503+AugustoCarrasco@users.noreply.github.com>
2023-09-23 23:58:43 +0000

    Base done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+let suits = ['♦', '♥','♠', '♣'];
+console.log(suits);
+let cards = ['Ace',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+console.log(cards);
+let numero = document.getElementsByClassName('numeros');
+console.log(numero);
+let palo = document.getElementById('palo');
+console.log(palo);
+window.onload = function getcard() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = Math.round(Math.random()*3);
+  let card = Math.round(Math.random()*12); 
+  if (suit <=1) {
+    palo.style.color ='red';
+    for (let i =0; i<numero.length; i ++) {
+      numero[i].style.color = 'red';
+    }
+    
+  }
+
+  for (let i =0; i<numero.length; i ++) {
+    numero[i].innerHTML = cards[card];
+  }
+
+  palo.innerHTML = suits[suit];
+  
+
+  return suit;
 };
