f8353e96adbaf70008b4b32538afb67687fe7cf6
Augusto Carrasco <142824503+AugustoCarrasco@users.noreply.github.com>
2023-10-18 08:57:36 +0000

    Saving

--- src/app.js
+++ src/app.js
@@ -1,113 +1,103 @@
-
-let suits = ['♦', '♥','♠', '♣'];
-let cards = ['Ace',2,3,4,5,6,7,8,9,10,'J','Q','K'];
-let numero = document.getElementsByClassName('numeros');
-let palo = document.getElementById('palo');
-let newcardbutton = document.getElementById('newcardbutton');
-let resizebutton = document.getElementById('resizebutton');
-let carta = document.getElementById('carta');
-let starttimer = document.getElementById('starttimer');
-let stoptimer = document.getElementById('stoptimer');
-let timercount = document.getElementById('timercount');
-let timerp = document.getElementById('timerp');
+let suits = ["♦", "♥", "♠", "♣"];
+let cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+let numero = document.getElementsByClassName("numeros");
+let palo = document.getElementById("palo");
+let newcardbutton = document.getElementById("newcardbutton");
+let resizebutton = document.getElementById("resizebutton");
+let carta = document.getElementById("carta");
+let starttimer = document.getElementById("starttimer");
+let stoptimer = document.getElementById("stoptimer");
+let timercount = document.getElementById("timercount");
+let timerp = document.getElementById("timerp");
 
 let timer;
 var myInterval;
 
 window.onload = function getcard() {
   //write your code here
-  let suit = Math.round(Math.random()*3);
-  let card = Math.round(Math.random()*12); 
-  if (suit <=1) {
-    palo.style.color ='red';
-    for (let i =0; i<numero.length; i ++) {
-      numero[i].style.color = 'red';
+  let suit = Math.round(Math.random() * 3);
+  let card = Math.round(Math.random() * 12);
+  if (suit <= 1) {
+    palo.style.color = "red";
+    for (let i = 0; i < numero.length; i++) {
+      numero[i].style.color = "red";
     }
-    
   }
 
-  for (let i =0; i<numero.length; i ++) {
+  for (let i = 0; i < numero.length; i++) {
     numero[i].innerHTML = cards[card];
   }
 
   palo.innerHTML = suits[suit];
-  
 
   return suit;
 };
 
 function getcard() {
   //write your code here
-  let suit = Math.round(Math.random()*3);
-  let card = Math.round(Math.random()*12); 
-  if (suit <=1) {
-    palo.style.color ='red';
-    for (let i =0; i<numero.length; i ++) {
-      numero[i].style.color = 'red';
+  let suit = Math.round(Math.random() * 3);
+  let card = Math.round(Math.random() * 12);
+  if (suit <= 1) {
+    palo.style.color = "red";
+    for (let i = 0; i < numero.length; i++) {
+      numero[i].style.color = "red";
     }
-    
   }
 
-  for (let i =0; i<numero.length; i ++) {
+  for (let i = 0; i < numero.length; i++) {
     numero[i].innerHTML = cards[card];
   }
 
   palo.innerHTML = suits[suit];
-  
 
   return suit;
 }
 
-newcardbutton.addEventListener('click', recargar);
+newcardbutton.addEventListener("click", recargar);
 
 function recargar() {
   location.reload();
 }
 
-starttimer.addEventListener('click', function() {
+starttimer.addEventListener("click", function() {
   countDown();
   timer = setInterval(getcard, 10000);
-  stoptimer.classList.remove('hidden');
-  timerp.classList.remove('hidden');
-  starttimer.classList.add('hidden');
-  
-  
-
+  stoptimer.classList.remove("hidden");
+  timerp.classList.remove("hidden");
+  starttimer.classList.add("hidden");
 });
 
-stoptimer.addEventListener('click', function() {
+stoptimer.addEventListener("click", function() {
   clearInterval(timer);
   clearInterval(myInterval);
-  stoptimer.classList.add('hidden');
-  timerp.classList.add('hidden');
-  starttimer.classList.remove('hidden');
+  stoptimer.classList.add("hidden");
+  timerp.classList.add("hidden");
+  starttimer.classList.remove("hidden");
 });
 
 function countDown() {
   var i = 9;
   var myInterval = setInterval(function() {
-      document.getElementById("timercount").innerHTML = i;
-  
-      if (i === 0) {
-        
-          i=9;
-          //call your function
-      }
-      else {
-          i--;
-         }
-      }, 1000);
-  }
+    document.getElementById("timercount").innerHTML = i;
 
-  var size = false;
-  resizebutton.addEventListener('click',function () {
-    if (!size) {
-      carta.classList.add('scale-125');
-      carta.classList.remove('scale-100');
-      size = true;
-    } else if (size) {
-      carta.classList.add('scale-100');
-      carta.classList.remove('scale-125');
-      size=false;
+    if (i === 0) {
+      i = 9;
+      //call your function
+    } else {
+      i--;
     }
-  });
+  }, 1000);
+}
+
+var size = false;
+resizebutton.addEventListener("click", function() {
+  if (!size) {
+    carta.classList.add("scale-125");
+    carta.classList.remove("scale-100");
+    size = true;
+  } else if (size) {
+    carta.classList.add("scale-100");
+    carta.classList.remove("scale-125");
+    size = false;
+  }
+});
